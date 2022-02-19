0864f18b0ce8b4f79b9ca3a72e6e513d0a7096a6
yousef7472 <yousef7shaban@gmail.com>
2022-02-19 22:42:36 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById('play').onclick = function () {
+    document.getElementById('vid').style.display='block'
+}
+
+closeVideo = function() {
+    document.getElementById('vid').style.display ='none';
+}
