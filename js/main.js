23ea1cfe87b38f86f909285f2f6be26ee9f9a294
yousef7472 <79384029+yousef7472@users.noreply.github.com>
2022-07-05 14:08:48 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ let loader = document.getElementById('load')
 window.addEventListener('load', function () {
     loader.style.display = 'none'
 })
+if(loader.style.display != 'none') {
+document.body.style.background-color = 'black';
+};
 
 document.getElementById('play').onclick = function () {
     document.getElementById('vid').style.display='block'
