21594e3fa7e2481c2a055eec57de6a7f1cb2eaf0
yousef7472 <79384029+yousef7472@users.noreply.github.com>
2022-07-05 12:00:54 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+let loader = document.getElementById('load')
+
+window.addEventListener('load', function () {
+    loader.style.display = 'none'
+})
+
 document.getElementById('play').onclick = function () {
     document.getElementById('vid').style.display='block'
 }
